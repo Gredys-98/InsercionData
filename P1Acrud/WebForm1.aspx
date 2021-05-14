51a50154b47aad2a14740dfb429336dcffd0cd6e
Gredys <gredypere9@gmail.com>
2021-05-14 13:29:02 -0600

    ConexionData

---
+++ P1Acrud/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="P1Acrud.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 233px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Button ID="SubirInformacion" runat="server" OnClick="SubirInformacion_Click" Text="Cargar Base De Datos" Height="59px" Width="184px" />
+    </form>
+</body>
+</html>
---
+++ P1Acrud/WebForm1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using P1Acrud.Clases.Archivos;
+using P1Acrud.Clases.BaseDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace P1Acrud
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CargarArchivoExterno()
+        {
+            string Fuente = ((@"C:\Users\alumno\Desktop\ArchivoCrudDB\crudDB.csv"));
+            ClsArchivos ar = new ClsArchivos();
+            //obtener todo el archivo linea por linea dentro de un arreglo simple--unidimensional
+            string[] ArregloNotas = ar.LeerArchivo(Fuente);
+            string sentencia_Mysql = "";
+            int NumeroLinea = 0;
+
+            ClsConecxion cn = new ClsConecxion();
+
+            foreach (string linea in ArregloNotas)
+            {
+                //obteniendo datos
+                string[] datos = linea.Split(';');
+                if(NumeroLinea > 0)
+                {
+                    sentencia_Mysql += $"insert into bases values({datos[0]}, '{datos[1]} ', {datos[2]}, {datos[3]}, {datos[4]}, {datos[5]}, '{datos[6]}'\n);";
+                }
+                NumeroLinea++;
+            }
+            NumeroLinea = 0;
+          
+            cn.EjecutaMySQLDirecto(sentencia_Mysql);
+        }
+       
+
+
+        protected void SubirInformacion_Click(object sender, EventArgs e)
+        {
+            CargarArchivoExterno();
+        }
+
+        protected void CargarMysql_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
